32c1c39b7349cc0a70104e5f7347067d497af08b
Ganesh Faterpekar <ganeshfaterpekar@Ganeshs-MacBook-Pro-2.local>
2021-08-25 16:22:26 +1000

    SPM Integration

---
+++ PointziSPMIntegration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PointziSPMIntegration
+//
+//  Created by Ganesh Faterpekar on 25/8/21.
+//
+
+import UIKit
+import StreetHawkCore_Pointzi
+
+class ViewController: StreetHawkBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
